995aee9d0a050fb04c57c7736692f6edfab3d70a
dmytr <dmytr@users.noreply.github.com>
2024-07-09 20:32:48 +0200

    ZIO 2.1 instrumentation

---
+++ dd-java-agent/instrumentation/zio/zio-2.1/src/main/java/datadog/trace/instrumentation/zio/v2_0/ZioRuntimeInstrumentation.java
@@ -0,0 +1,70 @@
+package datadog.trace.instrumentation.zio.v2_0;
+
+import static datadog.trace.agent.tooling.bytebuddy.matcher.NameMatchers.named;
+import static java.util.Collections.singletonMap;
+import static net.bytebuddy.matcher.ElementMatchers.isMethod;
+
+import com.google.auto.service.AutoService;
+import datadog.trace.agent.tooling.ExcludeFilterProvider;
+import datadog.trace.agent.tooling.Instrumenter;
+import datadog.trace.agent.tooling.InstrumenterModule;
+import datadog.trace.bootstrap.ContextStore;
+import datadog.trace.bootstrap.InstrumentationContext;
+import datadog.trace.bootstrap.instrumentation.java.concurrent.ExcludeFilter;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import net.bytebuddy.asm.Advice;
+import zio.Fiber;
+import zio.Supervisor;
+
+@AutoService(InstrumenterModule.class)
+public class ZioRuntimeInstrumentation extends InstrumenterModule.Tracing
+    implements Instrumenter.ForSingleType, ExcludeFilterProvider {
+
+  public ZioRuntimeInstrumentation() {
+    super("zio.experimental");
+  }
+
+  @Override
+  public String instrumentedType() {
+    return "zio.Runtime$";
+  }
+
+  @Override
+  protected final boolean defaultEnabled() {
+    return false;
+  }
+
+  @Override
+  public void methodAdvice(MethodTransformer transformer) {
+    transformer.applyAdvice(
+        isMethod().and(named("defaultSupervisor")), getClass().getName() + "$DefaultSupervisor");
+  }
+
+  @Override
+  public String[] helperClassNames() {
+    return new String[] {packageName + ".FiberContext", packageName + ".TracingSupervisor"};
+  }
+
+  @Override
+  public Map<String, String> contextStore() {
+    return singletonMap("zio.Fiber$Runtime", packageName + ".FiberContext");
+  }
+
+  @Override
+  public Map<ExcludeFilter.ExcludeType, ? extends Collection<String>> excludedClasses() {
+    return Collections.singletonMap(
+        ExcludeFilter.ExcludeType.RUNNABLE, Collections.singletonList("zio.internal.FiberRuntime"));
+  }
+
+  public static final class DefaultSupervisor {
+    @Advice.OnMethodExit(suppress = Throwable.class)
+    public static void onExit(@Advice.Return(readOnly = false) Supervisor<?> supervisor) {
+      @SuppressWarnings("rawtypes")
+      ContextStore<Fiber.Runtime, FiberContext> contextStore =
+          InstrumentationContext.get(Fiber.Runtime.class, FiberContext.class);
+      supervisor = supervisor.$plus$plus(new TracingSupervisor(contextStore));
+    }
+  }
+}
